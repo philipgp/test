e77ecc577a29175becd102633409db645afec9a4
philip <philipgp@outlook.com>
2017-05-20 00:04:46 +0300

    ffff

---
+++ src/main/java/org/philipgp/test/TestClass.java
@@ -0,0 +1,10 @@
+package org.philipgp.test;
+
+/**
+ * Created by philip on 19/5/17.
+ */
+public class TestClass {
+  public static void main(String args[]){
+      System.out.println("asdf123");
+  }
+}
